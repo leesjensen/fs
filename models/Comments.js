60e5f06a5fa964c87beddc04e87d4e9aad4baa4a
leesjensen <leesjensen@gmail.com>
2022-03-24 16:37:15 -0600

    add: rest endpoints

--- models/Comments.js
+++ models/Comments.js
@@ -5,4 +5,9 @@ const CommentSchema = new mongoose.Schema({
   upvotes: { type: Number, default: 0 },
 });
 
+CommentSchema.methods.upvote = function (cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
 mongoose.model('Comment', CommentSchema);
--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,47 @@ mongoose.connect(dbURL);
 const Comments = require('../models/Comments');
 const Comment = mongoose.model('Comment');
 
-/* GET home page. */
-router.get('/api', function (req, res, next) {
-  res.send('in index');
+router.param('comment', function (req, res, next, id) {
+  const query = Comment.findById(id);
+  query.exec(function (err, comment) {
+    if (err || !comment) {
+      return next(new Error(`can't find comment with id ${id}`));
+    }
+    req.comment = comment;
+    return next();
+  });
+});
+
+router.get('/comments', function (req, res, next) {
+  Comment.find(function (err, comments) {
+    if (err) {
+      return next(err);
+    }
+    res.json(comments);
+  });
+});
+
+router.post('/comments', function (req, res, next) {
+  const comment = new Comment(req.body);
+  comment.save(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
+    res.json(comment);
+  });
+});
+
+router.get('/comments/:comment', function (req, res) {
+  res.json(req.comment);
+});
+
+router.put('/comments/:comment/upvote', function (req, res, next) {
+  req.comment.upvote(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
+    res.json(comment);
+  });
 });
 
 module.exports = router;
