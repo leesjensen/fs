2eec871ec0a8532aac3ea7b656cba6872eb467e3
leesjensen <leesjensen@gmail.com>
2022-03-24 16:30:52 -0600

    add: mongo db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+const dbName = 'comments';
+
+const dbURL = `mongodb+srv://${userName}:${password}@${hostname}/${dbName}`;
+
+mongoose.connect(dbURL);
+
+const Comments = require('../models/Comments');
+const Comment = mongoose.model('Comment');
+
 /* GET home page. */
 router.get('/api', function (req, res, next) {
   res.send('in index');
